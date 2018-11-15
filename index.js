d10ba3e33aeb0303e01cdb10811524d86507785e
Miguel <miguelpacheson@hotmail.es>
2018-11-15 14:44:37 +0100

    added cart table

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ var vue = new Vue({
                 currency: "AUD",
                 description: "ANZA Workshop description text goes here",
                 earlybirdends: 20200320,
-                incart: false
+                incart: 0,
+                
             },
             {
                 name: "Berlin",
@@ -37,7 +38,7 @@ var vue = new Vue({
                 currency: "AUD",
                 description: "Berlin Workshop description text goes here",
                 earlybirdends: 20190311,
-                incart: false
+                incart: 0
             },
             {
                 name: "Beijing",
@@ -46,7 +47,7 @@ var vue = new Vue({
                 currency: "AUD",
                 description: "Beijing Workshop description text goes here",
                 earlybirdends: 20190311,
-                incart: false
+                incart: 0
             },
             {
                 name: "Miami",
@@ -55,7 +56,7 @@ var vue = new Vue({
                 currency: "AUD",
                 description: "Miami Workshop description text goes here",
                 earlybirdends: 20190311,
-                incart: false
+                incart: 0
             }
         ],
         addons: [
@@ -67,48 +68,62 @@ var vue = new Vue({
                 description: "Additional attendee without a schedule"
             }
         ],
+        headers:[
+            { text: 'Name', value: 'name', sortable: false },
+            {
+                text: 'Price',
+                value: 'price'
+            },
+            {
+                text: 'Qty',
+                value: 'qty'
+            },
+            {
+                text: 'SubTotal',
+                value: 'subTotal'
+            },
+        ], 
         cart: [], //it starts empty
         total: 0,
         saved: 0
     },
     methods: {
         addToCart: function (product) {
-            product.incart = true;
+            product.incart++;
             this.cart.push(product);
-            this.products = this.products.filter(m => { return m.product != product.product});
-            console.log(this.products)
-            let price, difference;
-            if (Date.now() > new Date(product.earlybirdends)) {
-                new Date(product.earlybirdends)
-                switch (this.cart.length) {
-                    case '1':
-                        price = product.price;
-                        difference = 0;
-                    case '2':
-                        price = product.price * 0.82;
-                        difference = product.price * 0.18;
-                        break;
-                    case '3':
-                        price = product.price * 0.8;
-                        difference = product.price * 0.2;
-                        break;
-                    case '4':
-                        price = product.price * 0.77;
-                        difference = product.price * 0.23;
-                        break;
-                    case '5':
-                        price = product.price * 0.75;
-                        difference = product.price * 0.25;
-                        break;
-                    default:
-                        price = product.price * 0.73;
-                        difference = product.price * 0.27;
-                }
-            }
-            else { price = priceearly };
-            this.total += price;
+            // this.products = this.products.filter(m => { return m.product != product.product});
+            // console.log(this.products)
+            // let price, difference;
+            // if (Date.now() > new Date(product.earlybirdends)) {
+            //     new Date(product.earlybirdends)
+            //     switch (this.cart.length) {
+            //         case '1':
+            //             price = product.price;
+            //             difference = 0;
+            //         case '2':
+            //             price = product.price * 0.82;
+            //             difference = product.price * 0.18;
+            //             break;
+            //         case '3':
+            //             price = product.price * 0.8;
+            //             difference = product.price * 0.2;
+            //             break;
+            //         case '4':
+            //             price = product.price * 0.77;
+            //             difference = product.price * 0.23;
+            //             break;
+            //         case '5':
+            //             price = product.price * 0.75;
+            //             difference = product.price * 0.25;
+            //             break;
+            //         default:
+            //             price = product.price * 0.73;
+            //             difference = product.price * 0.27;
+            //     }
+            // }
+            // else { price = priceearly };
+            this.total += product.price;
             this.saved += difference;
-
         },
         removeFromCart: function (product) {
             product.incart = false;
