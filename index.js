9760edfb45804f7573697038326cfd984423ee20
Miguel <miguelpacheson@hotmail.es>
2018-11-16 12:22:06 +0100

    added regularWorkshops property to vue object + changed padding of v-data-table + corrected one removeSubItem case

--- index.js
+++ index.js
@@ -20,102 +20,134 @@ const i18n = new VueI18n({
 var vue = new Vue({
     i18n,
     data: {
-        products: [
-					{
-                        id: 1,
-                        name: "ANZA",
-                        priceearly: 3400,
-                        price: 6900,
-                        currency: "AUD",
-                        description: "ANZA Workshop description text goes here",
-                        earlybirdends: 20200320,
-                        incart: 0,
-                        quantity: 0,
-                        subtotal: 0,
-                        tables: {
-                            id: 1,
-                            name: "ANZA Table",
-                            price: 6900,
-                            priceearly: 5600,
-                            quantity: 0,
-                            schedules: {
-                                    id: 1,
-                                    name: "ANZA Additional Schedule",
-                                    quantity: 0,
-                                    price: 2400
-                            },
-                            additionalPeople: {
-                                    id: 1,
-                                    name: "ANZA Additional Person",
-                                    quantity: 0,
-                                    price: 990
-                            },
-                        },
-                        additional: []
-                    },
-                    {
-                        id: 2,
-                        name: "Berlin",
-                        priceearly: 3900,
-                        price: 4400,
-                        currency: "EUR",
-                        description: "Berlin Workshop description text goes here",
-                        earlybirdends: 20190311,
-                        incart: 0,
-                        quantity: 0,
-                        subtotal: 0,
-                        tables: {
-                                id: 2,
-                                name: "Berlin Table",
-                                price: 4400,
-                                priceearly: 3900,
-                                quantity: 0,
-                                schedules: {
-                                        id: 2,
-                                        name: "Berlin Additional Schedule",
-                                        quantity: 0,
-                                        price: 2400
-                                },
-                                additionalPeople: {
-                                        id: 2,
-                                        name: "Berlin Additional Person",
-                                        quantity: 0,
-                                        price: 990
-                                },
-                        },
-                        additional: []
-                    },
-            {
-                id: 3,
-                name: "Beijing",
+        products: [{
+            id: 1,
+            name: "ANZA",
+            priceearly: 3400,
+            price: 6900,
+            currency: "AUD",
+            description: "ANZA Workshop description text goes here",
+            earlybirdends: 20200320,
+            incart: 0,
+            quantity: 0,
+            subtotal: 0,
+            tables: {
+                id: 1,
+                name: "ANZA Table",
                 price: 6900,
-                priceearly: 3900,
-                currency: "AUD",
-                description: "Beijing Workshop description text goes here",
-                earlybirdends: 20190311,
-                incart: 0,
-                attendance: {
-                    schedules: 0,
-                    additionalPeople: 0
+                priceearly: 5600,
+                quantity: 0,
+                schedules: {
+                    id: 1,
+                    name: "ANZA Additional Schedule",
+                    quantity: 0,
+                    price: 2400
                 },
-                additional: []
+                additionalPeople: {
+                    id: 1,
+                    name: "ANZA Additional Person",
+                    quantity: 0,
+                    price: 990
+                },
             },
-            {
-                id: 4,
-                name: "Miami",
-                price: 6900,
+            additional: []
+        },
+        {
+            id: 2,
+            name: "Berlin",
+            priceearly: 3900,
+            price: 4400,
+            currency: "EUR",
+            description: "Berlin Workshop description text goes here",
+            earlybirdends: 20190311,
+            incart: 0,
+            quantity: 0,
+            subtotal: 0,
+            tables: {
+                id: 2,
+                name: "Berlin Table",
+                price: 4400,
                 priceearly: 3900,
-                currency: "AUD",
-                description: "Miami Workshop description text goes here",
-                earlybirdends: 20190311,
-                incart: 0,
-                attendance: {
-                    schedules: 0,
-                    additionalPeople: 0
+                quantity: 0,
+                schedules: {
+                    id: 2,
+                    name: "Berlin Additional Schedule",
+                    quantity: 0,
+                    price: 2400
                 },
-                additional: []
-            }
-        ],
+                additionalPeople: {
+                    id: 2,
+                    name: "Berlin Additional Person",
+                    quantity: 0,
+                    price: 990
+                },
+            },
+            additional: []
+        },
+        {
+            id: 3,
+            name: "Beijing",
+            price: 4400,
+            priceearly: 3900,
+            currency: "EUR",
+            description: "Beijing Workshop description text goes here",
+            earlybirdends: 20190311,
+            incart: 0,
+            quantity: 0,
+            subtotal: 0,
+            tables: {
+                id: 2,
+                name: "Beijing Table",
+                price: 4400,
+                priceearly: 3900,
+                quantity: 0,
+                schedules: {
+                    id: 2,
+                    name: "Beijing Additional Schedule",
+                    quantity: 0,
+                    price: 2400
+                },
+                additionalPeople: {
+                    id: 2,
+                    name: "Beijing Additional Person",
+                    quantity: 0,
+                    price: 990
+                },
+            },
+            additional: []
+        },
+        {
+            id: 4,
+            name: "Miami",
+            price: 6900,
+            priceearly: 3900,
+            currency: "AUD",
+            description: "Miami Workshop description text goes here",
+            earlybirdends: 20190311,
+            incart: 0,
+            quantity: 0,
+            subtotal: 0,
+            tables: {
+                id: 2,
+                name: "Miami Table",
+                price: 4400,
+                priceearly: 3900,
+                quantity: 0,
+                schedules: {
+                    id: 2,
+                    name: "Miami Additional Schedule",
+                    quantity: 0,
+                    price: 2400
+                },
+                additionalPeople: {
+                    id: 2,
+                    name: "Miami Additional Person",
+                    quantity: 0,
+                    price: 990
+                },
+            },
+            additional: []
+        }],
         addons: [
             {
                 product: "Additional Person",
@@ -145,7 +177,8 @@ var vue = new Vue({
         ], 
         cart: [], //it starts empty
         total: 0,
-        saved: 0
+        saved: 0,
+        regularWorkshops: 0,
     },
     methods: {
         addToCart: function (product, subitem, selector) {
@@ -153,10 +186,14 @@ var vue = new Vue({
             this.cart.push(cartitem); //push copy of product into cart
             cartitem.price = subitem.price; //log price of subitem and assign it to copy's price
             cartitem.subtotal = subitem.price; //do same for subtotal (this one's subject to change)
-            this.total += subitem.price;
+            product.incart++;
+            this.regularWorkshops++;
+            if (this.regularWorkshops > 1){
+
+            }
+            else this.total += subitem.price;
             subitem.quantity++;
             cartitem.quantity++;
-            product.incart++;
         },
         removeFromCart: function (cartitem) {
             console.log(cartitem);
@@ -188,7 +225,7 @@ var vue = new Vue({
             if (selector == 'schedules'){
                 this.cart.forEach(m => {
                     if (m.id == product.id) {
-                        m.quantity = Math.floor((subitem.quantity / 2));
+                        m.quantity = Math.floor((subitem.quantity / 2)+1);
                         if (subitem.quantity == 1 || subitem.quantity == 0) m.quantity = 1;
                         if (subitem.quantity !== 0) this.total -= m.price;
                     }
@@ -216,6 +253,7 @@ var vue = new Vue({
             this.total -= product.price;
             var position = this.cart.indexOf(product);
             this.cart.splice(position, 1);
+            this.regularWorkshops--;
         }
     }
 }).$mount('#vue');
